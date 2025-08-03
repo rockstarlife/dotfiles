0910ac5857c2e89071ec7178267bf6f246e37261
Neo <chye.stas.stanislav@gmail.com>
2025-08-03 16:24:30 +0300

    minimal hypr

--- .config/nvim/init.lua
+++
@@ -1,5 +0,0 @@
-vim.opt.number = true         -- Показывать номера строк
-vim.opt.relativenumber = true -- Относительные номера строк
-vim.opt.tabstop = 2          -- 2 пробела для табуляции
-vim.opt.shiftwidth = 2       -- 2 пробела для отступов
-vim.opt.expandtab = true     -- Заменять табы пробелами
